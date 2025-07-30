3062db771923222d84c3e933065cf025cebf0f76
Yahya_Syauqoni25 <psyauqoni@gmail.com>
2025-07-30 14:32:58 +0700

    commit done yas

---
+++ Scripts/MinionB.gd
@@ -0,0 +1,78 @@
+extends CharacterBody3D
+
+var path = []
+var path_node = 0
+var speed = 4
+@onready var nav = get_parent()
+@onready var target = get_node("/root/World/MOBAmap/MainTowerB/Target")
+
+var move = true
+var attack = false
+
+var state
+enum{IDLE,RUN,ATTACK1,ATTACK2,ATTACK3}
+
+var anim = IDLE
+
+@onready var animate = $Char/AnimationTree
+
+func _ready():
+	state = IDLE
+
+func _on_Area_body_entered(body):
+	if body.is_in_group("towerA"):
+		move = false
+		attack = true
+		speed = 0
+		
+
+func _physics_process(delta):
+	animate()
+	if move == true:
+		attack = false
+	else:
+		attack = true
+	if attack == true:
+		move = false
+		change_state(ATTACK1)
+	
+	if path_node < path.size():
+		var direction = (path[path_node] - global_transform.origin)
+		if direction.length() < 1 :
+			path_node += 1
+		else:
+			set_velocity(direction.normalized() * speed)
+			set_up_direction(Vector3.UP)
+			move_and_slide()
+
+
+
+func move_to(target_pos):
+	path = nav.get_simple_path(global_transform.origin, target_pos)
+	path_node = 0
+	change_state(RUN)
+
+func _on_Timer_timeout():
+	if move == true:
+		move_to(target.global_transform.origin - Vector3(2,0,-2))
+		
+
+func change_state(new_state):
+	state = new_state
+	match state:
+		IDLE:
+			anim = IDLE
+		RUN:
+			anim = RUN
+		ATTACK1:
+			anim = ATTACK1
+		ATTACK2:
+			anim = ATTACK2
+		ATTACK3:
+			anim = ATTACK3
+	
+func animate():
+	animate["parameters/state/current"]=anim
+
+
+
---
+++ Scripts/World.gd
@@ -0,0 +1,52 @@
+extends Node3D
+
+
+func _on_Mtimer_timeout():
+	$Marker.visible = false
+	print("timeout")
+	
+	
+func _on_Terrain_input_event(camera, event, click_position, click_normal, shape_idx):
+	if (event is InputEventMouseButton and Input.is_mouse_button_pressed(1)):
+		$Marker.transform.origin = click_position
+		$Marker.visible = true
+		$Mtimer.start()
+		$Player.target = click_position
+		$Player/RayCast3D.enabled = false
+		$Player/RayCast2.enabled = false
+		$TurretMarker.visible = false
+		
+	elif event is InputEventMouseMotion and Input.is_mouse_button_pressed(1):
+		$Marker.transform.origin = click_position
+		$Marker.visible = true
+		$Mtimer.start()
+		$Player.target = click_position
+		$Player/RayCast3D.enabled = false
+		$Player/RayCast2.enabled = false
+
+
+func _on_StaticBody_input_event(camera, event, click_position, click_normal, shape_idx):
+	if (event is InputEventMouseButton and Input.is_mouse_button_pressed(1)):
+		$Player.target = get_object_under_mouse().collider.global_transform.origin
+		$Player/RayCast3D.enabled = true
+		$Player/RayCast2.enabled = true
+		$TurretMarker.global_transform.origin = get_object_under_mouse().collider.global_transform.origin
+		$TurretMarker.visible = true
+		
+func get_object_under_mouse():
+	var mouse_pos = get_viewport().get_mouse_position()
+	var ray_from = $Camera3D.project_ray_origin(mouse_pos)
+	var ray_to = ray_from + $Camera3D.project_ray_normal(mouse_pos) * 1000
+	var space_state = get_world_3d().direct_space_state
+	var selection = space_state.intersect_ray(ray_from, ray_to)
+	return selection
+
+func _on_MinionB_input_event(camera, event, click_position, click_normal, shape_idx):
+	if (event is InputEventMouseButton and Input.is_mouse_button_pressed(1)):
+		#$Player.target = get_object_under_mouse().collider.global_transform.origin
+		$Mtimer.start()
+		$TurretMarker.global_transform.origin = get_object_under_mouse().collider.global_transform.origin
+		$TurretMarker.visible = true
+
+
+
